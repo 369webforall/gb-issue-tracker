556756327fcbede4cd7f2fbc2da829cdc8d79589
369webforall <369webforall@gmail.com>
2025-07-21 14:39:01 +0545

    added css style for current path

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,17 +1,37 @@
+"use client";
 import Link from "next/link";
 import React from "react";
+import { usePathname } from "next/navigation";
 
 const Navbar = () => {
+  const currentPath = usePathname();
+  console.log(currentPath);
+  const links = [
+    {
+      href: "/dashboard",
+      label: "Dashboard",
+    },
+    {
+      href: "/issues",
+      label: "Issues",
+    },
+  ];
   return (
     <nav className="flex space-x-6 border-b px-10 py-6 mb-6">
       <Link href="/">Logo</Link>
       <ul className="flex space-x-6">
-        <li>
-          <Link href="/dashboard">Dashboard</Link>
-        </li>
-        <li>
-          <Link href="/issues">Issues</Link>
-        </li>
+        {links.map((issue, id) => (
+          <li key={id}>
+            <Link
+              href={issue.href}
+              className={`${
+                issue.href === currentPath ? "text-zinc-800" : "text-zinc-500"
+              } hover:text-zinc-900  transition-colors`}
+            >
+              {issue.label}
+            </Link>
+          </li>
+        ))}
       </ul>
     </nav>
   );
