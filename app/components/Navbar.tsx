0ad298c09390bb839856d7009878dfca87436f52
369webforall <369webforall@gmail.com>
2025-07-21 14:46:28 +0545

    add classnames library

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -2,6 +2,7 @@
 import Link from "next/link";
 import React from "react";
 import { usePathname } from "next/navigation";
+import classnames from "classnames";
 
 const Navbar = () => {
   const currentPath = usePathname();
@@ -20,15 +21,17 @@ const Navbar = () => {
     <nav className="flex space-x-6 border-b px-10 py-6 mb-6">
       <Link href="/">Logo</Link>
       <ul className="flex space-x-6">
-        {links.map((issue, id) => (
+        {links.map((link, id) => (
           <li key={id}>
             <Link
-              href={issue.href}
-              className={`${
-                issue.href === currentPath ? "text-zinc-800" : "text-zinc-500"
-              } hover:text-zinc-900  transition-colors`}
+              href={link.href}
+              className={classnames({
+                "text-zinc-900": link.href === currentPath,
+                "text-zinc-500": link.href !== currentPath,
+                "hover:text-zinc-800 transition-colors": true,
+              })}
             >
-              {issue.label}
+              {link.label}
             </Link>
           </li>
         ))}
